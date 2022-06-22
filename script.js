c2fa6880b3cf0c1711c68acc747d6c766db9f306
thomasprayon <thomasprayongowland@gmail.com>
2022-06-22 19:12:49 +0200

    make basic code to get at least the convertion from binary to decimal

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 'use strict';
-console.log('script attached to HTML file');
+const inputValue = document.getElementById('binaryToDecimal');
+const decimalBtn = document.querySelector('.btn');
+const result = document.getElementById('resultToDecimal');
+
+decimalBtn.addEventListener('click', () => {
+    console.log('click happening');
+    let totalDecimal = 0;
+    const getValue = inputValue.value;
+    const makeItArr = getValue
+        .split('')
+        .reverse()
+        .forEach((element, index) => {
+            if (element === '0' || element === '1') {
+                const calcBinary = element * 2 ** index;
+                totalDecimal += calcBinary;
+                result.textContent = totalDecimal;
+            } else {
+                result.textContent = 'Please enter 1 or 0 characters';
+            }
+        });
+});
